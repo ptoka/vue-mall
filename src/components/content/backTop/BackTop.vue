5fd6d1ae7149e77c39972b46915824b85f536a16
ptoka <plutojts@126.com>
2020-03-17 16:55:01 +0800

    'backtop'

---
+++ src/components/content/backTop/BackTop.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="back-top">
+    <img src="~assets/img/common/top.png"
+         alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BackTop"
+  }
+</script>
+
+<style scope>
+  .back-top {
+    position: fixed;
+    bottom: 60px;
+    right: 10px;
+  }
+  .back-top img {
+    height: 43px;
+    width: 43px;
+  }
+</style>
--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -3,44 +3,52 @@
     <nav-bar class="index-nav">
       <template v-slot:center>购物街</template>
     </nav-bar>
-    <home-child-swiper :banners="banners"></home-child-swiper>
-    <recommend-view :recommends="recommend"></recommend-view>
-    <tab-control ref="tabControl1" :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
-    <goods-list :goods="goodsType"></goods-list>
+    <scroll class="content"
+            ref="scroll">
+      <home-child-swiper :banners="banners"></home-child-swiper>
+      <recommend-view :recommends="recommend"></recommend-view>
+      <tab-control ref="tabControl1"
+                   :titles="['流行','新款','精选']"
+                   @tabClick="tabClick"></tab-control>
+      <goods-list :goods="goodsType"></goods-list>
+    </scroll>
+    <back-top @click.native="backClick"></back-top>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
-  import {getHomeMultidata, getHomeGoods} from 'network/home'
+  import { getHomeMultidata, getHomeGoods } from 'network/home'
   import HomeChildSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
   import TabControl from 'components/content/tabControl/TabControl'
-  import GoodList from  'components/content/goods/GoodsList'
+  import GoodList from 'components/content/goods/GoodsList'
   import GoodsList from "../../components/content/goods/GoodsList";
+  import Scroll from "components/common/scroll/Scroll";
+  import BackTop from "components/content/backTop/BackTop";
 
   export default {
     name: 'Index',
-    data() {
+    data () {
       return {
         banners: [],
         recommend: [],
         keywords: [],
         dKeyword: null,
-        index:0,
+        index: 0,
         goods: {
-          pop: {'page': 0, 'list': []},
-          new: {'page': 0, 'list': []},
-          sell: {'page': 0, 'list': []}
+          pop: { 'page': 0, 'list': [] },
+          new: { 'page': 0, 'list': [] },
+          sell: { 'page': 0, 'list': [] }
         },
         currentType: ["pop", "new", "sell"],
       }
     },
     components: {
       GoodsList,
-      NavBar, HomeChildSwiper, RecommendView, TabControl,GoodList
+      NavBar, HomeChildSwiper, RecommendView, TabControl, GoodList, Scroll, BackTop
     },
-    created() {
+    created () {
       this.getHomeMultidata();
 
       this.getHomeGoods("pop");
@@ -50,7 +58,7 @@
     },
     methods: {
       /*网络请求*/
-      getHomeMultidata() {
+      getHomeMultidata () {
         getHomeMultidata().then(res => {
           this.banners = res.data.banner.list;
           this.recommend = res.data.recommend.list;
@@ -60,7 +68,7 @@
           console.log(this.recommend)
         })
       },
-      getHomeGoods(type) {
+      getHomeGoods (type) {
         const page = this.goods[type].page + 1;
         getHomeGoods(type, page).then(res => {
           this.goods[type].list.push(...res.data.list);
@@ -69,14 +77,17 @@
         })
       },
       /*事件监听*/
-      tabClick(index){
+      tabClick (index) {
         this.index = index;
         // this.$refs.tabControl1.currentIndex = index;
         // this.$refs.tabControl2.currentIndex = index;
+      },
+      backClick () {
+        this.$refs.scroll.scrollTo(0, 0)
       }
     },
-    computed:{
-      goodsType() {
+    computed: {
+      goodsType () {
         return this.goods[this.currentType[this.index]].list;
       }
     }
@@ -84,8 +95,21 @@
 </script>
 
 <style scoped>
+  #index {
+    height: 100vh;
+    position: relative;
+  }
   .index-nav {
     background-color: #ff5777;
     color: #fff;
+  }
+  .content {
+    /* height: calc(100%-93px); */
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 44px;
+    bottom: 49px;
+    overflow: hidden;
   }
 </style>
